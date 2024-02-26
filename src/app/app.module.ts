d870de1b2c98a5676bdc2a33b7f86c23fee7ecbc
wesley <euweslei17@gmail.com>
2024-02-26 14:43:22 -0300

    dando erro na import do quizz_questions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuizComponent } from './components/quiz/quiz.component';
 import { HomeComponent } from './page/home/home.component';
+
 
 @NgModule({
   declarations: [
